22707fceb0d4c3812da880360365327488ce50c8
smjcarlos96 <smjcarlos96@gmail.com>
2020-11-10 03:19:07 +0100

    3.28. Class methods and arrow functions

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ class App extends Component {
       monsters: [],
       searchField: '',
     };
+
+    /*
+      3. bind returns a copy of the said function with a new definition for "this" keyword, in this case App class itself, and it's done like:
+      this.handleChange = this.handleChange.bind(this);
+      As handleChange is defined in App scope, we can refer to it with "this" from constructor. But "this" inside bind function passes App as the scope to use.
+    */
   }
 
   componentDidMount() {
@@ -20,6 +26,18 @@ class App extends Component {
       .then(response => response.json())
       .then(json => this.setState({ monsters: json }));
   }
+
+  /*
+    2. There are two ways to do so. First one is a normal function like:
+    handleChange(e) { this.setState({searchField: e.target.value}) }
+    and setting SearchBox attribute to {hangleChange} (no need to put brackets, it will pass the event object likewise).
+    But will be needed to set the meaning for "this" keyword from the function scope to the class scope. Look for comment 3.
+
+    Second way is to define an arrow function like:
+    const handleChange = (e) => {this.setState({searchField: e.target.value})}
+    and setting SearchBox attribute only to {handleChange}.
+    The reason why "this" works is because anonymous functions are defined under class scope and not function scope. 
+  */
 
   render() {
     const { monsters, searchField } = this.state;
@@ -30,6 +48,7 @@ class App extends Component {
 
     return (
       <div className='App'>
+        {/* 1. Here in SearchBox we define handleChange inside the tag but we can also define it outside. Chack comment number 2. */}
         <SearchBox placeholder='search monsters' handleChange={e => { this.setState({ searchField: e.target.value }) }} />
         <CardList monsters={filteredMonsters} />
       </div>
--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 
 import './search-box.css';
 
-// This is a functional component. It consists of a constant-anonymous function that returns JSX code.
-// Functional components do not extend from Component, so they don't have access to state, constructor, life cycle methods.
-// They're simple components that don't need any resources and just return JSX.
-// As well as components only get one parameter named props it's necessary to destructure it for easier access to props' properties.
 export const SearchBox = ({ placeholder, handleChange }) => (
   <input type='search' className='search' placeholder={placeholder} onChange={handleChange} />
 );
